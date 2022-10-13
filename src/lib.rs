ae30d5e5c029f9413c2477a145560c2f7ca923dc
cfran <cfranssens34@gmail.com>
2022-10-13 08:04:52 -0400

    Update README.md (I update this a lot)

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+// General (applies to all networks)
+// TODO: Layer trait
+// TODO: Dataset struct
+
+// Feedforward network
+// TODO: Activation enum
+// TODO: Loss Function
+// TODO: Optimizers
+// TODO: Dropout
+
+// Convolutional Network
+// TODO: Convolutional Layer struct
+// TODO: Filter struct with size, stride, and padding arguments
+// TODO: Max Pooling layer
+// TODO: Other things in a convolutional network
+
+// Recurrent Network
+// TODO: LSTM implementation (idk a lot about this type of network)
+
+
+// Visualisation
+// TODO: Boring print functions for now until I have the networks figured out.
+// TODO: Cool graphs displaying how terrible a network is doing with WebGpu-rs
